911c3adeb90f473d4d0682ad3de6a2ae7055eb42
Sionparadox <sion9999@naver.com>
2025-04-10 05:30:10 +0900

    [design] 기본 global.css 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,16 @@ const config: Config = {
       fontFamily: {
         sans: ["Pretendard Variable", "Pretendard", "-apple-system", "BlinkMacSystemFont", "system-ui", "Roboto", "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "sans-serif"],
       },
+      colors: {
+        primary: "#61B89F",
+        deepPrimary: "#4E8F77",
+        secondary: "#D3EBCD",
+        tertiary: "#B1CDE2",
+        main: "#F8F8F8",
+        sub: "#FDFDFD",
+        text:"#464646",
+        gray: "#AAAAAA",
+      },
     },
   },
   plugins: [],
